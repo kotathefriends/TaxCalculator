28cfe7ae7ee54435e9d91fef8df0c42cb8369779
kotathefriends <5070kouta@gmail.com>
2023-10-14 16:23:10 +0900

    Initial Commit

---
+++ TaxCalculator/TaxCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaxCalculatorApp.swift
+//  TaxCalculator
+//
+//  Created by KOTA TAKAHASHI on 2023/10/14.
+//
+
+import SwiftUI
+
+@main
+struct TaxCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
